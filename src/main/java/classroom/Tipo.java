a2db924935ee36b5ff41b1074e33f9df01be5a75
Ju4n3sD3v <juanherrera@unal.edu.co>
2024-11-26 12:23:24 -0500

    Se arreglaron todos los errores de código o estructurales

--- src/main/java/classroom/Tipo.java
+++ src/main/java/classroom/Tipo.java
@@ -1,12 +1,15 @@
 package classroom;
 
+
 public enum Tipo {
 	DISCIPLINAR(10),FUNDAMENTACION(20),ELECTIVA(30);
 	
-	private int codigo;
-	private String nombre;
-	public Tipo(int codigo,String nombre) {
+	private int codigo; /*Se elimino el atributo nombre */
+	private Tipo(int codigo) { /*Se cambión de público a privado */
 		this.codigo = codigo;
-		this.nombre = nombre;
+	}
+
+	public int getCodigo() { /*Opcional: creé un getter para codigo */
+		return this.codigo;
 	}
 }
